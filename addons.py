45f28231cd64ac7f1c8540ba45468193d1b8277d
feznapling <seb@fez.net.au>
2025-03-19 00:11:50 +0800

    main file enable/disable code

--- addons.py
+++ addons.py
@@ -4,11 +4,18 @@ from pathlib import Path
 def ig_f(dir, files):
     return [f for f in files if os.path.isfile(os.path.join(dir, f))]
 
-def add_addon():
+def enable_addon():
+    #addon_name = 'evilmansion_fixed'
+    addon_name = 'coolmod'
     working_dir = Path(os.getcwd())
-    addons_dir = Path(working_dir / 'addons')
-    game_dir = Path(working_dir / 'game')
+    addons_dir = Path.joinpath(working_dir, 'addons')
+    game_dir = Path.joinpath(working_dir, 'game')
 
+    addon_src = Path.joinpath(addons_dir, addon_name)
+    target = Path(game_dir)
+    symlinks=False
+
+    active_addons_file = os.path.abspath(addons_dir) + "\\active_addons.txt"
     
     if game_dir.exists():
         print("game dir exists")
@@ -20,28 +27,81 @@ def add_addon():
 
         os.mkdir(addons_dir)
 
-    #src = Path(addons_dir / 'coolmod')
-    src = Path(addons_dir / 'evilmansion_fixed')
-    target = Path(game_dir)
-    symlinks=False
+    # try:
+    #     with open(active_addons_file, 'r') as file:
+    #         file.seek(0)
+    #         if addon_name in file.read():
+    #             print(f"addon '{addon_name}' already active")
+    #             sys.exit()
+    # except Exception as e:
+    #     print(f'Error: {e}')
+
 
     #Create directory structure
-    for dir in os.listdir(src):
-        file_source = os.path.join(src, dir)
-        file_target = os.path.join(target, dir)
+    for dir in os.listdir(addon_src):
+        file_source = Path.joinpath(addon_src, dir)
+        file_target = Path.joinpath(target, dir)
         if os.path.isdir(file_source):
             shutil.copytree(file_source, file_target, symlinks, dirs_exist_ok=True, ignore=ig_f)
 
     #Copy files
-    for root, dirs, files in os.walk(src, topdown=True):
+    for root, dirs, files in os.walk(addon_src, topdown=True):
         #print(os.path.basename(root))
-        relative_path = os.path.relpath(root, src)
-        file_target = os.path.join(target, relative_path)
+        relative_path = os.path.relpath(root, addon_src)
+        file_target = Path.joinpath(target, relative_path)
 
         for file in files:
             shutil.copy2(os.path.join(root, file), os.path.join(file_target, file))
             print(f"copying {os.path.join(relative_path, file)} to {file_target}...")
 
+    try:
+        with open(active_addons_file, 'a') as file:
+            file.write(addon_name + '\n')
+            print(f"'{addon_name}' added to {os.path.relpath(active_addons_file, addons_dir)}")
+    except Exception as e:
+        print(f'Error: {e}')
+
+def disable_addon():
+    #addon_name = 'evilmansion_fixed'
+    addon_name = 'coolmod'
+    working_dir = Path(os.getcwd())
+    addons_dir = Path.joinpath(working_dir, 'addons')
+    game_dir = Path.joinpath(working_dir, 'game')
+
+    addon_src = Path.joinpath(addons_dir, addon_name)
+    target = Path(game_dir)
+    symlinks=False
+
+    active_addons_file = os.path.abspath(addons_dir) + "\\active_addons.txt"
+
+    file_list = []
+
+    for root, dirs, files in os.walk(addon_src, topdown=True):
+        #print(os.path.basename(root))
+        relative_path = os.path.relpath(root, addon_src)
+        file_target = Path.joinpath(target, relative_path)
+
+        for file in files:
+            file_list.append(file)
+    
+    
+
+    for root, dirs, files in os.walk(game_dir, topdown=False):
+
+        for file in files:
+            if file in file_list:
+                os.remove(os.path.join(root, file))
+                print(f"removing file {file}...")
+
+
+        for dir in dirs:
+            current_dir = os.path.join(root, dir)
+            empty = os.listdir(current_dir)
+            if len(empty) == 0:
+                Path.rmdir(current_dir)
+                print(f"removed directory {current_dir}...")
+
 
 if __name__ == "__main__":
-    add_addon()
+    #enable_addon()
+    disable_addon()
